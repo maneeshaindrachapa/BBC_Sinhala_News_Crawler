12ebda6de34d0379cad9b33576ac7dc0c4e89b09
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2019-06-29 12:47:30 +0530

    BBC sinhala news Crawler

---
+++ sinhala_dataset/spiders/BBC_sinhala_news.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BbcSinhalaNewsSpider(scrapy.Spider):
+    name = 'BBC_sinhala_news'
+    allowed_domains = ['https://www.bbc.com']
+    start_urls = ['https://www.bbc.com/sinhala/sport','https://www.bbc.com/sinhala/world','https://www.bbc.com/sinhala/topics/f6ec89fd-3823-498e-a888-572e96f791b2','https://www.bbc.com/sinhala/topics/0f469e6a-d4a6-46f2-b727-2bd039cb6b53','https://www.bbc.com/sinhala/topics/e45cb5f8-3c87-4ebd-ac1c-058e9be22862','https://www.bbc.com/sinhala/sri_lanka']
+    custom_settings={ 'FEED_URI': "BBC_sinhala%(time)s.json",
+                    'FEED_FORMAT': 'json'}
+    def parse(self, response):
+        print("procesing:"+response.url)
+        #Extract data using css selectors
+        #title = response.xpath("//div[@class='budgie-item faux-block-link']/div[@class='budgie__body']/a[@class='title-link']/h3[@class='title-link__title']/span[@class='title-link__title-text']/text()").extract()
+        title=response.css('.title-link__title-text::text').extract()
+        description=response.css('.eagle-item__summary::text').extract()
+        
+        #Extract data using xpath
+        #orders=response.xpath("//em[@title='Total Orders']/text()").extract()
+        #company_name=response.xpath("//a[@class='store $p4pLog']/text()").extract()
+
+        row_data=zip(title,description)
+
+        #Making extracted data row wise
+        for item in row_data:
+            #create a dictionary to store the scraped info
+            scraped_info = {
+                #key:value
+                'page':response.url,
+                'title' : item[0], #item[0] means product in the list and so on, index tells what value to assign
+                'description' : item[1],
+            }
+
+            #yield or give the scraped info to scrapy
+            yield scraped_info
